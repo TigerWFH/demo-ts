dd011b248618c3987cd8856ca4ff02b94ff36fe3
monkey-office <monkey@lwork.com>
2017-03-15 19:26:23 +0800

    add appFooter

--- src/components/appFooter.tsx
+++ src/components/appFooter.tsx
@@ -5,19 +5,24 @@ import * as ReactDOM from 'react-dom';
 // components and utils
 // models
 
-interface FooterProps { }
+interface FooterProps {
+    content?: React.ReactNode | string;
+}
 interface FooterState { }
 
 export class AppFooter extends React.Component<FooterProps, FooterState>{
-    refs: any;
+    static defaultProps = {
+        content: "appFooter"
+    };
     constructor(props: FooterProps) {
         super(props);
+        this.state = {};
     }
 
     render() {
         return (
-            <div>
-                appFooter
+            <div className="appFooter">
+                {this.props.content}
             </div>
         )
     }
