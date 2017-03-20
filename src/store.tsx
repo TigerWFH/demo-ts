917de4f7b0a6c5f5692f311c5ecbe2a4758bb8ed
monkey-office <monkey@lwork.com>
2017-03-20 11:26:29 +0800

    暂时替换回来

--- src/store.tsx
+++ src/store.tsx
@@ -1,9 +1,11 @@
 import { createStore } from 'redux';
-// import handleActions from './module1/reducers';
-import { reducers } from './reducers';
+import handleActions from './module1/reducers';
+// import { reducers } from './reducers';
+// let store = createStore(reducers);
 // store的api:subscribe(),dispatch(),getState()
 // 根据reducers创建store
-let store = createStore(reducers);
+
+let store = createStore(handleActions);
 store.subscribe(() => {
     console.log('store update', store.getState())
 })
